7c66c27b0fd502d446b098c9febab2a1ee3ba245
RamsesMassalim <r.massalim@gmail.com>
2022-07-08 12:16:20 +0600

    first commit

---
+++ src/test/java/kz/halykacademy/bookstoreproject/BookstoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.halykacademy.bookstoreproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
